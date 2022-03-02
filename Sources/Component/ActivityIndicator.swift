16fd4726f54f8c23ba37a3f3b0667ac3832f2ec9
Shaw <cuzval@gmail.com>
2022-03-02 10:36:04 +0800

    Update ActivityIndicator

--- Sources/Component/ActivityIndicator.swift
+++ Sources/Component/ActivityIndicator.swift
@@ -5,15 +5,23 @@ import UIKit
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
 public struct ActivityIndicator: UIViewRepresentable {
   private let style: UIActivityIndicatorView.Style
+  private let color: UIColor?
   @Binding private var isAnimating: Bool
 
-  public init(style: UIActivityIndicatorView.Style, isAnimating: Binding<Bool>) {
+  public init(
+    style: UIActivityIndicatorView.Style,
+    color: UIColor? = nil,
+    isAnimating: Binding<Bool> = .constant(true)
+  ) {
     self.style = style
+    self.color = color
     self._isAnimating = isAnimating
   }
 
   public func makeUIView(context: Context) -> UIActivityIndicatorView {
-    .init(style: style)
+    let view = UIActivityIndicatorView(style: style)
+    view.color = color
+    return view
   }
 
   public func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
