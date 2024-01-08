91c2b18d1688a5fc9e4ab499c9f47d63c6059218
Shaw <cuzval@gmail.com>
2024-01-08 16:11:12 +0800

    Add default args

--- Sources/Extension/Stack+.swift
+++ Sources/Extension/Stack+.swift
@@ -2,8 +2,12 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, macCatalyst 13.0, *)
 public extension View {
-   func inHStack(justify: HorizontalAlignment = .leading, spacing: CGFloat? = nil) -> some View {
-    HStack(spacing: spacing) {
+  func inHStack(
+    justify: HorizontalAlignment = .leading,
+    align: VerticalAlignment = .center,
+    spacing: CGFloat? = nil
+  ) -> some View {
+    HStack(alignment: align, spacing: spacing) {
       if [.trailing, .center].contains(justify) {
         Spacer()
       }
@@ -13,9 +17,13 @@ public extension View {
       }
     }
   }
-
-  func inVStack(justify: VerticalAlignment = .top, spacing: CGFloat? = nil) -> some View {
-    VStack(spacing: spacing) {
+  
+  func inVStack(
+    justify: VerticalAlignment = .top,
+    alignment: HorizontalAlignment = .center,
+    spacing: CGFloat? = nil
+  ) -> some View {
+    VStack(alignment: alignment, spacing: spacing) {
       if [.bottom, .center].contains(justify) {
         Spacer()
       }
@@ -29,8 +37,12 @@ public extension View {
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, macCatalyst 14.0, *)
 public extension View {
-  func inLazyHStack(justify: HorizontalAlignment = .leading, spacing: CGFloat? = nil) -> some View {
-    LazyHStack(spacing: spacing) {
+  func inLazyHStack(
+    justify: HorizontalAlignment = .leading,
+    align: VerticalAlignment = .center,
+    spacing: CGFloat? = nil
+  ) -> some View {
+    LazyHStack(alignment: align, spacing: spacing) {
       if [.trailing, .center].contains(justify) {
         Spacer()
       }
@@ -40,9 +52,13 @@ public extension View {
       }
     }
   }
-
-  func inLazyVStack(justify: VerticalAlignment = .top, spacing: CGFloat? = nil) -> some View {
-    LazyVStack(spacing: spacing) {
+  
+  func inLazyVStack(
+    justify: VerticalAlignment = .top,
+    alignment: HorizontalAlignment = .center,
+    spacing: CGFloat? = nil
+  ) -> some View {
+    LazyVStack(alignment: alignment, spacing: spacing) {
       if [.bottom, .center].contains(justify) {
         Spacer()
       }
